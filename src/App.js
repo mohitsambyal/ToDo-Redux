79571466d722e85306bccbb6eed59db96f5b9fc0
msambyal <msambyal@texala.net>
2022-12-29 21:21:26 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AddTodoAction, RemoveTodoAction } from "./actions/TodoActions";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const [todo, setTodo] = useState();
+  const todoData = useSelector((state) => state.Todo);
+  const { todos } = todoData;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo));
+  };
+  const removeHandler = (data) => {
+    dispatch(RemoveTodoAction(data));
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Todo List</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name=""
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 10,
+              border: "none",
+              fontSize: 20,
+            }}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            type="submit"
+            style={{
+              padding: 12,
+              borderRadius: 25,
+              fontSize: 15,
+              marginLeft: 20,
+            }}
+          >
+            Go
+          </button>
+          <ul
+            style={{
+              listStyle: "none",
+              margin: 0,
+              padding: 0,
+            }}
+          >
+            {todos &&
+              todos.map((item) => (
+                <li key={item.id}>
+                  <span>{item.todo}</span>
+                  <button
+                    style={{
+                      padding: 10,
+                      borderRadius: 25,
+                      fontSize: 15,
+                      marginLeft: 20,
+                      border: "1px solid white",
+                      color: "white",
+                      backgroundColor: "#e44444",
+                    }}
+                    onClick={() => removeHandler(item)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+          </ul>
+        </form>
       </header>
     </div>
   );
---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { TodoReducers } from "./reducers/TodoReducers";
+
+const reducer = combineReducers({
+  // this contains all reducers
+  Todo: TodoReducers,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+export default store;
